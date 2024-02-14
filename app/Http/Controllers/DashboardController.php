2f9c38563bda743ae5113b505849fcc3d4468c86
Nirmal Dhapodkar <nirmald759@gmail.com>
2024-02-14 17:54:25 +0530

    Made changes CRUD
    made a migration and model with some view changes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+
     public function index(){
-        return view('dashboard');
+        $idea = new Idea([
+            'content' => "hello Youtube"
+        ]);
+        $idea -> save();
+
+        // dump(Idea::all());
+        return view('dashboard',['ideas' => Idea::orderBy('created_at', 'DESC')->get()]);
     }
 }
