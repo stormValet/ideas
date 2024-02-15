553ccb70ef9f4ff7accf7cdde1224907fdc48802
Storm Valet <valetviscount@gmail.com>
2024-02-15 14:57:44 +0530

    Adding Pagination
    Added Pagination to show only 5 cards of post per page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,6 +12,8 @@ class DashboardController extends Controller
     {
 
         // dump(Idea::all());
-        return view('dashboard', ['ideas' => Idea::orderBy('created_at', 'DESC')->get()]);
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->paginate(5)
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,7 +47,9 @@
             @include('shared.idea-card')
         </div>
         @endforeach
-
+        <div class="mt-3">
+        {{ $ideas->links() }}
+        </div>
     </div>
     <div class="col-3">
         <div class="card">
